7ca12bb0a2ea86bb50575d9dddce2b8aca6445ab
Giorgos Charitakis <chgiorgos13@gmail.com>
2022-10-24 19:45:16 +0300

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "AdverticalTV-ads",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v9),
+        .tvOS(.v13),
     ],
     products: [
         .library(name: "AdverticalTV-ads", targets: ["AdverticalTV_ads"])
@@ -12,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdverticalTV_ads",
-            url: "https://github.com/arx-net/AdverticalTV-ads-iOS/releases/download/1.0.1/xcframework.zip",
-            checksum: "4ec11917183465ec307f437b82058262e314d26e13a7d6cca643ea164d7e11c0"
+            url: "https://github.com/arx-net/AdverticalTV-ads-iOS/releases/download/1.1.0/xcframework.zip",
+            checksum: "ace1a7a6d21580d4b1f1bb3f86f2af58fcbf65547aa46ce3539f1ad32a1cf2cd"
         )
     ],
     swiftLanguageVersions: [.v4, .v4_2, .v5]
